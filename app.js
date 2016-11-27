cef2ba26762b1bf10cc4beac269c8804ec35b729
gavinning <ningyubo@gmail.com>
2016-11-27 16:44:28 +0800

    v1.0.0

---
+++ app.js
@@ -0,0 +1,35 @@
+// Alidayu SMS
+
+class Alidayu {
+
+    constructor(clientOption, smsOption) {
+        this.Client = require( './sdk/topClient' ).TopClient;
+        this.clientOption = clientOption;
+        this.smsOption = smsOption;
+        this.client = new this.Client(clientOption);
+    }
+
+    send(tel, options) {
+        let opt = {};
+
+        if(typeof tel === 'object'){
+            options = tel;
+            tel = options.tel;
+        }
+        if(!tel){
+            return console.log('需要一个手机号码')
+        }
+        opt.name = options.name;
+        opt.time = options.time;
+        this.smsOption.rec_num = tel;
+        this.smsOption.sms_param = JSON.stringify(opt);
+
+        return new Promise((res, rej) => {
+            this.client.execute('alibaba.aliqin.fc.sms.num.send', this.smsOption, function(error, response) {
+                 error ? rej(error) : res(response)
+            });
+        })
+    }
+}
+
+module.exports = Alidayu;
